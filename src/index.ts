8feeb92125a65d31a2881ae4667b9f792bd5c409
Jay Hernandez <jeric614@yahoo.com>
2021-09-27 18:11:55 +0900

    read csv file w/fs and parsed

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('STARTED')
+//when importing node modules, install type definition file
+//npm install @types/node
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+console.log(matches);
