8b7e90123bc9f108765e1d7cf6ddc4e6e4e6499d
Jay Hernandez <jeric614@yahoo.com>
2021-09-28 13:56:56 +0900

    app complete, generated reports html and implemented static methods f/preconfiguration

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 //when importing node modules, install type definition file
 //npm install @types/node
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
+// import { CsvFileReader } from './CsvFileReader';
+// import { ConsoleReport } from './reportTargets/ConsoleReport';
+// import { WinsAnalysis } from './analyzers/WinsAnalysis';
+// import { HtmlReport } from './reportTargets/HtmlReports';
 
 //create object that satisfied 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
+// const csvFileReader = new CsvFileReader('football.csv');
 //create instance of MatchReader and pass in var created
-const matchReader = new MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
 matchReader.load();
-
-
-
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
